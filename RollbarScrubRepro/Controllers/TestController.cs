5fe65061aa0dde1012b386c241ab7662515dcb95
BradM <brad@4pi.com.au>
2019-04-18 15:06:19 +1000

    Initial commit.

---
+++ RollbarScrubRepro/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Rollbar;
+using Rollbar.NetCore.AspNet;
+using RollbarScrubRepro.Data;
+
+namespace RollbarScrubRepro.Controllers
+{
+    public class TestController : ControllerBase
+    {
+        [Route("api/test")]
+        [HttpPost]
+        public ActionResult Get([FromBody] LoginData loginData)
+        {
+            try
+            {
+                throw new Exception("Some error happened.");
+            }
+            catch (Exception err)
+            {
+                var rollbarPackage = new ExceptionPackage(err);
+                var packageDecorator = new HttpRequestPackageDecorator(rollbarPackage, HttpContext.Request, true);
+                RollbarLocator.RollbarInstance.Log(ErrorLevel.Error, packageDecorator);
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ RollbarScrubRepro/Data/LoginData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace RollbarScrubRepro.Data
+{
+    public class LoginData
+    {
+        [JsonProperty("username")]
+        public string Username { get; set; }
+
+        [JsonProperty("password")]
+        public string Password { get; set; }
+    }
+}
